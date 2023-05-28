1b63b15fcae88f86b4ca7b414979b6ac38388c5c
Artur Arirokk Bondarenko <skullbull@protonmail.com>
2023-05-28 14:18:24 +0400

    devices-list table

--- scripts/dynamodb-create-table.sh
+++ scripts/dynamodb-create-table.sh
@@ -2,8 +2,14 @@
 
 aws dynamodb create-table \
   --table-name drones-devices \
-  --cli-input-json file:///scripts/schema.json \
+  --cli-input-json file:///dynamodb/schema-drones-devices.json \
   --endpoint-url http://database:7999 \
+  --region us-east-1
+
+aws dynamodb create-table \
+  --table-name devices-list \
+  --cli-input-json file:///dynamodb/schema-drones-devices.json \
+  -endpoint-url http://database:7999 \
   --region us-east-1
 
 tail -f /dev/null
